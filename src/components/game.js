9095bc6759021c11df6369e973a56b632f8fd959
trentduncan <trent.dunc@gmail.com>
2018-04-12 08:45:26 -0700

    implemented taking user guess and changing state

--- src/components/game.js
+++ src/components/game.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentTarget: 55,
+            currentGuess: false,
+            previousGuesses: [],
+            possibleFeedback: ['Make your guess!', 'cold','less than warm','warm', 'warmer', 'hot', 'SCORCHING', 'YOU WON'],
+            currentFeedback: ['Make your guess!']
+        }
+    }
+    
+    setCurrentGuess(currentGuess) {
+        this.setState({currentGuess});
+    }
+
+
+    render () {
+        return (
+            <div>
+                <Header />
+                <GuessSection currentGuess={guess => this.setCurrentGuess(guess)} feedback="Make your guess!" />
+                <GuessCount count={3} />
+                <GuessList guesses={[10, 15, 25]} />
+            </div>
+        );
+    }
 }
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -4,7 +4,17 @@ import './guess-form.css';
 
 export default function GuessForm(props) {
     return (
-        <form>
+        <form onSubmit={event => {
+            event.preventDefault();
+            const guess = event.target.userGuess.value;
+            if (guess > 100 || guess < 1) {
+                alert('Guess must be between 1 and 100!!!')
+            } else {
+                props.currentGuess(event.target.userGuess.value);
+                event.target.userGuess.value = '';
+            }
+            
+        }}>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
                 placeholder="Enter your Guess" required />
